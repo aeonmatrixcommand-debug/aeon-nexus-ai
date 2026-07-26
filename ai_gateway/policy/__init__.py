46db4201d24262ec888e06fc8420995f8db83888
aeonmatrixcommand-debug <kannuttida2@gmail.com>
2026-07-26 13:49:49 +0000

    fix: restore policy guard compatibility

--- ai_gateway/policy/__init__.py
+++ ai_gateway/policy/__init__.py
@@ -2,3 +2,13 @@
 from ai_gateway.policy.engine import PolicyEngine
 from ai_gateway.policy.decision import PolicyDecision
 from ai_gateway.policy.rules import PolicyRules
+
+
+class PolicyGuard:
+
+    def __init__(self):
+        self.engine = PolicyEngine()
+
+    def check(self, action):
+        return self.engine.evaluate(action)
+
